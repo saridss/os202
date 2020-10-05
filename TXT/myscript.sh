bf0c671ce23d3f08eb61226bb6d29294479dbec9
saridss <71247398+saridss@users.noreply.github.com>
2020-10-05 18:01:59 +0700

    Create myscript.sh

---
+++ TXT/myscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#ok
+
+FILES="my*.txt my*.sh"
+SHA="SHA256SUM"
+
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
